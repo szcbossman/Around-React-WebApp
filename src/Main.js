ea68518ef94309bc4d75b2049c1228af7e31fe2b
szcbossman <songzicheng3@gmail.com>
2018-01-10 21:25:17 -0800

    Register component added

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Register } from './Register'
 
 export class Main extends React.Component {
     render() {
         return (
-            <div>this is main</div>
+            <div>
+                <Register/>
+            </div>
         );
     }
 }
