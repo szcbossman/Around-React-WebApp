bd4956088f258f5234846b11eb97d9330950fc05
szcbossman <songzicheng3@gmail.com>
2018-01-10 21:15:55 -0800

    Around webapp with React, antDesign added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Header } from './Header'
+import './App.css';
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+            <Header/>
+      </div>
+    );
+  }
+}
+
+export default App;
