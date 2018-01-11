35051cfa0cb99f8e9d286acd00dac6388478bf67
szcbossman <songzicheng3@gmail.com>
2018-01-10 21:19:49 -0800

    Main component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Header } from './Header'
+import { Main } from './Main'
 import './App.css';
 
 
@@ -8,7 +9,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-            <Header/>
+          <Header/>
+          <Main/>
       </div>
     );
   }
