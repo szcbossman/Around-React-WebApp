80ae5ff2a84898ea06fc23386886a2f6e5b89b3b
szcbossman <songzicheng3@gmail.com>
2018-01-15 21:59:33 -0800

    isLoggedIn state added

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,19 @@ import '../styles/App.css';
 
 
 class App extends React.Component {
+  state = {
+    isLoggedIn: false,
+  }
+
+  loginHandler = (response) => {
+    this.setState({ isLoggedIn: true});
+  }
+
   render() {
     return (
       <div className="App">
         <Header/>
-        <Main/>
+        <Main loginHandler = {this.loginHandler} isLoggedIn = {this.state.isLoggedIn}/>
       </div>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,16 +2,26 @@ import React from 'react'
 import { Register } from './Register'
 import { Login } from './Login'
 import { Switch, Route } from 'react-router-dom'
+import { Home } from './Home'
 
 export class Main extends React.Component {
+  getLogin = () => {
+    return this.props.isLoggedIn ? <Home/> : <login loginHandler={this.props.loginHandler}/>;
+  }
+
+  getHome = () => {
+    return this.props.isLoggedIn ? <Home/> : <login loginHandler={this.props.loginHandler}/>;
+  }
+
   render() {
     return (
       <div className='main'>
         <Switch>
-          <Route exact path="/" component={Login}/>
+          <Route exact path="/" render={this.getLogin}/>
           <Route path="/register" component={Register}/>
-          <Route path="/login" component={Login}/>
-          <Route component={Login}/>
+          <Route path="/login" render={this.getLogin}/>
+          <Route path="/home" render={this.getHome} />
+          <Route render={this.getLogin}/>
         </Switch>
       </div>
     );
