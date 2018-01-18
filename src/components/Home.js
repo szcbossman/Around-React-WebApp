6725965def31ca4b6db2c7f66c0104311e701e78
szcbossman <songzicheng3@gmail.com>
2018-01-17 20:57:42 -0800

    loadNearbyPosts function structure added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import $ from 'jquery';
 import { Spin } from 'antd';
 import { Tabs, Button } from 'antd';
-import { GEO_OPTIONS } from '../constants';
+import {API_ROOT, GEO_OPTIONS, POS_KEY, AUTH_PREFIX, TOKEN_KEY} from '../constants';
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
@@ -33,7 +34,11 @@ export class Home extends React.Component {
   }
 
   onSuccessLoadGeoLocation = (position) => {
+    console.log(position);
     this.setState({ loadingGeoLocation: false, error: '' });
+    const {latitude, longitude} = position.coords;
+    localStorage.setItem('POS_KEY', JSON.stringify({ lat: latitude, lon: longitude }));
+    this.loadNearbyPosts();
   }
   onFailedLoadGeoLocation = () => {
     this.setState({ loadingGeoLocation: false, error: 'Failed to load geoLocation!' });
@@ -49,6 +54,23 @@ export class Home extends React.Component {
     else {
       return null;
     }
+  }
+
+  loadNearbyPosts = () => {
+    const {lat, lon} = JSON.parse(localStorage.getItem(POS_KEY));
+    $.ajax({
+      url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20`,
+      method: 'GET',
+      headers: {
+        Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`
+      },
+    }).then((response) => {
+      console.log(response);
+    }, (error) => {
+      console.log(error);
+    }).catch((error) => {
+      console.log(error)
+    });
   }
 
   render() {
