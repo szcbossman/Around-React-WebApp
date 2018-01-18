2ffe91ecea613e77fdd1fd661708ecf658dad127
szcbossman <songzicheng3@gmail.com>
2018-01-17 19:23:42 -0800

    Home Tabs styles added

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,13 +7,10 @@ const operations = <Button>Extra Action</Button>;
 export class Home extends React.Component {
   render() {
     return (
-      <div>
-        <Tabs tabBarExtraContent={operations}>
-          <TabPane tab="Tab 1" key="1">Content of tab 1</TabPane>
-          <TabPane tab="Tab 2" key="2">Content of tab 2</TabPane>
-          <TabPane tab="Tab 3" key="3">Content of tab 3</TabPane>
-        </Tabs>
-      </div>
+      <Tabs tabBarExtraContent={operations} className = "main-tabs">
+        <TabPane tab="Posts" key="1">Content of tab 1</TabPane>
+        <TabPane tab="Map" key="2">Content of tab 2</TabPane>
+      </Tabs>
     );
   }
 }
