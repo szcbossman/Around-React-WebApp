63680c802e04e0e5dce3b90afb8c20b794c9cac7
szcbossman <songzicheng3@gmail.com>
2018-01-22 20:31:35 -0800

    second item in submit button form added

--- src/components/CreateButtonForm.js
+++ src/components/CreateButtonForm.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import { Form, Input } from 'antd';
+import { Form, Input, Upload, Icon } from 'antd';
 const FormItem = Form.Item;
 
 class CreateButtonForm extends React.Component {
+  normFile = (e) => {
+    console.log('Upload event:', e);
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e && e.fileList;
+  }
+
   render() {
     const { getFieldDecorator } = this.props.form;
     const formItemLayout = {
@@ -20,6 +28,26 @@ class CreateButtonForm extends React.Component {
           })(
             <Input />
           )}
+        </FormItem>
+        <FormItem
+          {...formItemLayout}
+          label="Image"
+        >
+          <div className="dropbox">
+            {getFieldDecorator('image', {
+              valuePropName: 'fileList',
+              getValueFromEvent: this.normFile,
+              rules: [{ required: true, message: 'Please select an image.' }],
+            })(
+              <Upload.Dragger name="files" action="/upload.do">
+                <p className="ant-upload-drag-icon">
+                  <Icon type="inbox" />
+                </p>
+                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+              </Upload.Dragger>
+            )}
+          </div>
         </FormItem>
       </Form>
     );
