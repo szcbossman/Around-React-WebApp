fc55c97fb57d8f1ee49dc9634f555f3882892eca
szcbossman <songzicheng3@gmail.com>
2018-01-24 20:22:58 -0800

    localStorage loc for map center and get posts from Home's state

--- src/components/Home.js
+++ src/components/Home.js
@@ -113,6 +113,7 @@ export class Home extends React.Component {
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `400px` }} />}
             mapElement={<div style={{ height: `100%` }} />}
+            posts={this.state.posts}
           />
         </TabPane>
       </Tabs>
